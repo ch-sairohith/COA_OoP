35747e80c5f76074ba426ea02336559f3c6f5def
nikhil-muvvala <nikhilmuvvala2@gmail.com>
2026-03-02 16:01:55 +0530

    Create Instruction class for opcode handling

---
+++ utils/Instruction.py
@@ -0,0 +1,10 @@
+
+
+class Instruction:
+    def __init__(self,opcode,pc,rd=None,rs1=None,rs2=None,imm=None):
+        self.opcode = opcode
+        self.pc = pc
+        self.rd = rd
+        self.rs1 = rs1
+        self.rs2 = rs2
+        self.imm = imm
