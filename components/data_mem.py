6e0bf50cd3418bb7fd1df036fac5f326ab7750a4
nikhil-muvvala <nikhilmuvvala2@gmail.com>
2026-03-03 17:48:14 +0530

    Implement Memory class with read and store methods

---
+++ components/data_mem.py
@@ -0,0 +1,67 @@
+
+class Memory:
+
+    def __init__(self,size=4096):
+        self.size=size
+        self.mem=bytearray(size)
+        self.base_address=0
+
+    def read_byte(self,address):
+
+        if address >= self.size:
+            raise Exception("Memory out of bounds")
+
+        return self.mem[address]
+    
+    def store_byte(self,address,val):
+
+        if address >= self.size:
+            raise Exception("Memory out of bounds")
+
+        self.mem[address]=val & 255
+
+    def read_word(self,address):
+        if(address%4!=0):
+            raise Exception("The starting address for memory access is multiple of 4")
+        if address + 4 > self.size:
+            raise Exception("Memory out of bounds")
+        
+        value = 0
+
+        for i in range(4):
+            value += self.mem[address+i]<<(8*i)
+
+        return value
+    
+    def store_word(self,address,val):
+        if(address%4!=0):
+            raise Exception("The starting address for memory access is multiple of 4")
+        if address + 4 > self.size:
+            raise Exception("Memory out of bounds")
+        
+        for i in range(4):
+            self.mem[address+i]=val & 255
+            val=val>>8
+
+    def data_section_word(self,val):
+
+        if self.base_address%4 != 0:
+            self.base_address +=4-self.base_address%4
+
+        if self.base_address + 4 > self.size:
+            raise Exception("Memory out of bounds")
+        
+        for i in range(4):
+            self.mem[self.base_address+i]=val & 255
+            val=val>>8
+
+        self.base_address+=4
+    
+    def data_section_byte(self,val):
+
+        if self.base_address >= self.size:
+            raise Exception("Memory out of bounds")
+        
+        self.mem[self.base_address]=val
+        self.base_address+=1         
+        
