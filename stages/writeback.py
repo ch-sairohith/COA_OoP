2ce797c0f2266c088ae03af6a666297a57d5473f
nikhil-muvvala <nikhilmuvvala2@gmail.com>
2026-03-04 12:15:22 +0530

    Implement Writeback class with step method

---
+++ stages/writeback.py
@@ -0,0 +1,13 @@
+class Writeback:
+
+    def step(self, mem_wb_latch, registers, stall):
+
+        if stall or mem_wb_latch.is_nop:
+            return
+        
+        if mem_wb_latch.reg_write:
+
+            if mem_wb_latch.mem_to_reg:
+                registers.write(mem_wb_latch.rd_addr, mem_wb_latch.mem_data)
+            else:
+                registers.write(mem_wb_latch.rd_addr, mem_wb_latch.alu_result)
