6096a57f50e2bf881c72389a23a31ed97223489d
ch-sairohith <chsairohith14@gmail.com>
2026-03-07 23:56:51 +0530

    Enhance hazard detection with additional forwarding
    Added forwardC and forwardD for hazard detection and updated stall conditions for different stages.

--- core/hazard_unit.py
+++ core/hazard_unit.py
@@ -2,6 +2,8 @@ class HazardUnit:
     def detect(self, if_id, id_ex, ex_mem, mem_wb):
         forwardA = "NONE"
         forwardB = "NONE"
+        forwardC="NONE"
+        forwardD="NONE"
         stall = False
 
         if not ex_mem.is_nop and not id_ex.is_nop and ex_mem.reg_write and not ex_mem.mem_read and ex_mem.rd_addr != 0:
@@ -18,7 +20,7 @@ class HazardUnit:
  
         if not id_ex.is_nop and not if_id.is_nop and id_ex.alu_op=="lw" and id_ex.rd_addr!=0:
             if id_ex.rd_addr==if_id.instruction.rs1 or id_ex.rd_addr==if_id.instruction.rs2:
-                stall=True
+                stall="decode"
 
         if not if_id.is_nop and if_id.instruction.opcode in ["beq", "bne"]:
             branch_rs1 = if_id.instruction.rs1
@@ -26,10 +28,19 @@ class HazardUnit:
 
             if not id_ex.is_nop and id_ex.reg_write and id_ex.rd_addr != 0:
                 if id_ex.rd_addr == branch_rs1 or id_ex.rd_addr == branch_rs2:
-                    stall = True
-
-            if not ex_mem.is_nop and ex_mem.reg_write and ex_mem.rd_addr != 0:
+                    stall = "decode" 
+            if not ex_mem.is_nop and not ex_mem.mem_read and ex_mem.reg_write:
+                if ex_mem.rd_addr == branch_rs1:
+                    forwardC = "ex_mem"
+                if ex_mem.rd_addr == branch_rs2:
+                    forwardD = "ex_mem"
+            if not mem_wb.is_nop and  mem_wb.mem_to_reg :
+                if mem_wb.rd_addr == branch_rs1:
+                    forwardC = "mem_wb"
+                if mem_wb.rd_addr == branch_rs2:
+                    forwardD = "mem_wb"        
+            if not ex_mem.is_nop and ex_mem.reg_write and ex_mem.rd_addr != 0 and  ex_mem.mem_read:
                 if ex_mem.rd_addr == branch_rs1 or ex_mem.rd_addr == branch_rs2:
-                    stall = True
+                    stall = "execute"
 
-        return forwardA, forwardB, stall
+        return forwardA, forwardB,forwardC,forwardD, stall
